87644283a87689cac48ff141a4cc1c2728c335c6
Art Raut <info@artraut.com>
2019-04-23 15:53:16 +0300

    configure environment

---
+++ src/components/Aviasales.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface AviasalesProps { compiler: string; framework: string; }
+
+// 'AviasalesProps' describes the shape of props.
+// State is never set so we use the '{}' type.
+export class Aviasales extends React.Component<AviasalesProps, {}> {
+    render() {
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
